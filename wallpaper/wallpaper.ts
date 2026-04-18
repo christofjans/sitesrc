17c27075f98cb47f9a711e5e6aa3ba56e5e6e801
Christof Jans <christofjans@gmail.com>
2026-04-18 17:24:41 -0400

    wallpaper

--- wallpaper/wallpaper.ts
+++ wallpaper/wallpaper.ts
@@ -1,38 +1,46 @@
-export function initMainTimeWidget(maintimeid) {
+export function initMainTimeWidget(maintimeid: string) {
     let maintimeWidget = document.getElementById(maintimeid);
-    setInterval(function () {
-        if (maintimeWidget === null)
-            return;
+    
+    setInterval(function(){
+        if (maintimeWidget===null) return;
         let date = new Date();
         let msg = `${padLeft(date.getHours())}:${padLeft(date.getMinutes())}`;
         maintimeWidget.innerHTML = msg;
     }, 400);
+
 }
-export function initMainDateWidget(maindateid) {
+
+export function initMainDateWidget(maindateid: string) {
     let maindateWidget = document.getElementById(maindateid);
-    if (maindateWidget === null)
-        return;
+    if (maindateWidget===null) return;
+
     let date = new Date();
+
     let dayOfWeekIdx = date.getDay();
     let dayOfMonth = date.getDate();
     let monthIdx = date.getMonth();
+    
     let today = `${days[dayOfWeekIdx]}, ${months[monthIdx]} ${dayOfMonth}`;
     maindateWidget.innerHTML = today;
 }
-export function initUtcTimeWidget(utctimeid) {
+
+export function initUtcTimeWidget(utctimeid: string) {
     let utctimeWidget = document.getElementById(utctimeid);
-    setInterval(function () {
-        if (utctimeWidget === null)
-            return;
+
+    setInterval(function(){
+        if (utctimeWidget===null) return;
         let date = new Date();
         let msg = `UTC ${padLeft(date.getUTCHours())}:${padLeft(date.getUTCMinutes())}`;
         utctimeWidget.innerHTML = msg;
     }, 400);
+
 }
-function padLeft(i) {
-    return i < 10 ? `0${i}` : `${i}`;
+
+function padLeft(i: number) : string {
+    return i<10 ? `0${i}` : `${i}`;
 }
-const days = [
+
+const days: string[] = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -41,7 +49,8 @@ const days = [
     "Friday",
     "Saturday"
 ];
-const months = [
+
+const months: string[] = [
     "January",
     "February",
     "March",
